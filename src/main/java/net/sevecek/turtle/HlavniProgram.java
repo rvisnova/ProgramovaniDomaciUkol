e6fd67cedb956b58eedcdc77efa68587c9b6ed11
Radka Višňová <radka.visnova@gmail.com>
2021-10-25 23:19:02 +0200

    Prevod metod (trojuhelnik, ctverec, obdelnik, kolecko) na parametrizovane metody

--- src/main/java/net/sevecek/turtle/HlavniProgram.java
+++ src/main/java/net/sevecek/turtle/HlavniProgram.java
@@ -3,38 +3,43 @@ package net.sevecek.turtle;
 import net.sevecek.turtle.engine.*;
 
 public class HlavniProgram {
-
+    Turtle zofka;
     public void main(String[] args) {
-        Turtle zofka;
 
         zofka = new Turtle();
 
-         //Rovnoramenny trojuhelnik
-//        zofka.move(150);
-//        zofka.turnLeft(141.058);
-//        zofka.move(150);
-//        zofka.turnLeft(109.471);
-//        zofka.move(100);
-//        zofka.turnLeft(109.471);
+        zofka.setLocation(350, 300);
+        nakresliRovnoramennyTrojuhelnik(150, 100, 109.471, 141.058);
 
-        //Ctverec
-//        for (int i = 0; i <4; i++){
-//            zofka.move(100);
-//            zofka.turnRight(90);
-//        }
-
-        //Obdelnik
-//        for (int i = 0; i < 2; i++) {
-//            zofka.move(150);
-//            zofka.turnRight(90);
-//            zofka.move(100);
-//            zofka.turnRight(90);
-//        }
-
+        }
+    public void nakresliCtverec(double velikostStrany) {
+        for (int i = 0; i <4; i++){
+            zofka.move(velikostStrany);
+            zofka.turnRight(90);
+        }
+    }
+    public void nakresliObdelnik(double velikostStranyA,
+                                 double velikostStranyB) {
+        for (int i = 0; i < 2; i++) {
+            zofka.move(velikostStranyA);
+            zofka.turnRight(90);
+            zofka.move(velikostStranyB);
+            zofka.turnRight(90);
+        }
+    }
+    public void nakresliKolecko(double polomer){
         for (int i = 0; i<360; i++) {
-            zofka.move(100 * 0.0174);
+            zofka.move(polomer * 0.0174);
             zofka.turnRight(1);
         }
-
-        }
+    }
+    public void nakresliRovnoramennyTrojuhelnik(double stranaA, double stranaC,
+                                                double uhelAlfa, double uhelGama){
+                zofka.move(stranaA);
+        zofka.turnLeft(uhelGama);
+        zofka.move(stranaA);
+        zofka.turnLeft(uhelAlfa);
+        zofka.move(stranaC);
+        zofka.turnLeft(uhelAlfa);
+    }
     }
