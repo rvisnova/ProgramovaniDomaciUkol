e0242256ec0cb9b65a8b55dd4d6d6ce43a1a486c
Radka Višňová <radka.visnova@gmail.com>
2021-10-25 22:08:13 +0200

    Vytvoreni rovnoramenneho trojuhelniku s pevnymi velikostmi stran a uhlu

---
+++ src/main/java/net/sevecek/turtle/HlavniProgram.java
@@ -0,0 +1,19 @@
+package net.sevecek.turtle;
+
+import net.sevecek.turtle.engine.*;
+
+public class HlavniProgram {
+
+    public void main(String[] args) {
+        Turtle zofka;
+
+        zofka = new Turtle();
+
+        zofka.move(150);
+        zofka.turnLeft(141.058);
+        zofka.move(150);
+        zofka.turnLeft(109.471);
+        zofka.move(100);
+        zofka.turnLeft(109.471);
+        }
+    }
