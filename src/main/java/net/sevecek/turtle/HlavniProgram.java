e5dfe2aaf37e0e64333dd6a3514798ca35842cb6
Radka Višňová <radka.visnova@gmail.com>
2021-10-26 23:54:52 +0200

    Nakresleni snehulaka

--- src/main/java/net/sevecek/turtle/HlavniProgram.java
+++ src/main/java/net/sevecek/turtle/HlavniProgram.java
@@ -4,42 +4,74 @@ import net.sevecek.turtle.engine.*;
 
 public class HlavniProgram {
     Turtle zofka;
+
     public void main(String[] args) {
 
         zofka = new Turtle();
 
-        zofka.setLocation(350, 300);
-        nakresliRovnoramennyTrojuhelnik(150, 100, 109.471, 141.058);
-
-        }
-    public void nakresliCtverec(double velikostStrany) {
-        for (int i = 0; i <4; i++){
-            zofka.move(velikostStrany);
-            zofka.turnRight(90);
-        }
-    }
-    public void nakresliObdelnik(double velikostStranyA,
-                                 double velikostStranyB) {
-        for (int i = 0; i < 2; i++) {
-            zofka.move(velikostStranyA);
-            zofka.turnRight(90);
-            zofka.move(velikostStranyB);
-            zofka.turnRight(90);
-        }
-    }
-    public void nakresliKolecko(double polomer){
-        for (int i = 0; i<360; i++) {
-            zofka.move(polomer * 0.0174);
+        zofka.setLocation(300, 350);
+        nakresliKolecko(80);
+        zofka.penUp();
+        for (int i = 0; i < 90; i++) {
+            zofka.move(80 * 0.0174);
             zofka.turnRight(1);
         }
+        zofka.turnLeft(180);
+        zofka.penDown();
+        nakresliKolecko(60);
+        zofka.penUp();
+        for (int i = 0; i < 90; i++) {
+            zofka.move(60 * 0.0174);
+            zofka.turnRight(1);
+        }
+        zofka.turnLeft(180);
+        zofka.penDown();
+        nakresliKolecko(20);
+        zofka.penUp();
+        for (int i = 0; i < 180; i++) {
+            zofka.move(60 * 0.0174);
+            zofka.turnLeft(1);
+        }
+        zofka.penDown();
+        nakresliKolecko(20);
+        zofka.penUp();
+        for (int i = 0; i < 90; i++) {
+            zofka.move(60 * 0.0174);
+            zofka.turnLeft(1);
+        }
+        zofka.penDown();
+        nakresliKolecko(40);
+
+
     }
-    public void nakresliRovnoramennyTrojuhelnik(double stranaA, double stranaC,
-                                                double uhelAlfa, double uhelGama){
-                zofka.move(stranaA);
-        zofka.turnLeft(uhelGama);
-        zofka.move(stranaA);
-        zofka.turnLeft(uhelAlfa);
-        zofka.move(stranaC);
-        zofka.turnLeft(uhelAlfa);
-    }
-    }
+        public void nakresliCtverec ( double velikostStrany){
+            for (int i = 0; i < 4; i++) {
+                zofka.move(velikostStrany);
+                zofka.turnRight(90);
+            }
+        }
+        public void nakresliObdelnik ( double velikostStranyA,
+        double velikostStranyB){
+            for (int i = 0; i < 2; i++) {
+                zofka.move(velikostStranyA);
+                zofka.turnRight(90);
+                zofka.move(velikostStranyB);
+                zofka.turnRight(90);
+            }
+        }
+        public void nakresliKolecko ( double polomer){
+            for (int i = 0; i < 360; i++) {
+                zofka.move(polomer * 0.0174);
+                zofka.turnRight(1);
+            }
+        }
+        public void nakresliRovnoramennyTrojuhelnik ( double stranaA, double stranaC,
+        double uhelAlfa, double uhelGama){
+            zofka.move(stranaA);
+            zofka.turnLeft(uhelGama);
+            zofka.move(stranaA);
+            zofka.turnLeft(uhelAlfa);
+            zofka.move(stranaC);
+            zofka.turnLeft(uhelAlfa);
+        }
+}
