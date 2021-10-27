e79c01016f0c20315fdd8a3304b1fe3000d2a178
Radka Višňová <radka.visnova@gmail.com>
2021-10-27 15:06:51 +0200

    Nakresleni masiny

--- src/main/java/net/sevecek/turtle/HlavniProgram.java
+++ src/main/java/net/sevecek/turtle/HlavniProgram.java
@@ -8,8 +8,8 @@ public class HlavniProgram {
     public void main(String[] args) {
 
         zofka = new Turtle();
-
-//        zofka.setLocation(300, 350);
+//
+//        zofka.setLocation(250, 350);
 //        nakresliKolecko(80);
 //        zofka.penUp();
 //        for (int i = 0; i < 90; i++) {
@@ -42,22 +42,49 @@ public class HlavniProgram {
 //        zofka.penDown();
 //        nakresliKolecko(40);
 //        zofka.turnRight(90);
+//
+//        zofka.setLocation(50, 260);
+//        nakresliKolecko(68);
+//
+//        zofka.penUp();
+//        zofka.turnRight(180);
+//        for (int i = 0; i < 15; i++) {
+//            zofka.move(68 * 0.0174);
+//            zofka.turnLeft(1);
+//        }
+//        zofka.turnLeft(7.5);
+//        zofka.penDown();
+//        nakresliRovnoramennyTrojuhelnik(171.44, 131.2, 112.5, 135);
+//        zofka.turnLeft(157.5);
 
-        zofka.setLocation(50, 230);
-        nakresliKolecko(68);
-        //85
+        zofka.setLocation(700, 350);
+        nakresliKolecko(52);
+
+        nakresliObdelnik(150, 104);
+        zofka.turnLeft(90);
+
+        nakresliObdelnik(184, 75);
+        zofka.penUp();
+        zofka.move(61);
+        zofka.turnLeft(180);
+        zofka.penDown();
+
+        nakresliKolecko(28);
+        zofka.turnLeft(180);
+        zofka.move(75);
+        zofka.turnLeft(180);
+        nakresliKolecko(28);
 
         zofka.penUp();
-        zofka.turnRight(180);
-        for (int i = 0; i < 15; i++) {
-            zofka.move(68 * 0.0174);
-            zofka.turnLeft(1);
-        }
-        zofka.turnLeft(7.5);
+        zofka.turnLeft(180);
+        zofka.move(48);
+        zofka.turnLeft(90);
+        zofka.move(25);
+        zofka.turnRight(90);
+        zofka.move(84);
+        zofka.turnLeft(180);
         zofka.penDown();
-        nakresliRovnoramennyTrojuhelnik(171.44, 131.2, 112.5, 135);
-        //214.3, 164
-        zofka.turnLeft(157.5);
+        nakresliRovnoramennyTrojuhelnik(84, 118.8, 135, 90);
 
     }
         public void nakresliCtverec ( double velikostStrany){
